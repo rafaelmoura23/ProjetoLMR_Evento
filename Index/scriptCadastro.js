3c8b94b6269bef32307a6b122986c6ff1f7333ea
rafaelmoura23 <rafaelzinmoura23@gmail.com>
2023-06-10 13:45:26 -0300

    10.06

--- Index/scriptCadastro.js
+++ Index/scriptCadastro.js
@@ -1,11 +1,14 @@
 function cadastrar(){
-    
+    let nome = document.getElementById("pNome").value;
+    let sobreNome = document.getElementById("pLastName").value;
+
+    alert("Welcome " +nome+" "+sobreNome+ "!")
+    window.location.href = "index.html";
 }
 
 
 function sair(){
     let deslogar = document.getElementById("deslogar").value;
-
     alert("You logged out!")
     window.location.href = "index.html";
 }
