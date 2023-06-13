789a3b80a18828a3df0cb7129eeb281b1707f1de
Rafael Moura <123770371+rafaelmoura23@users.noreply.github.com>
2023-06-13 19:19:58 +0000

    13.06

--- Index/scriptCadastro.js
+++ Index/scriptCadastro.js
@@ -1,9 +1,21 @@
 function cadastrar(){
     let nome = document.getElementById("pNome").value;
     let sobreNome = document.getElementById("pLastName").value;
+    let email = document.getElementById("pEmail").value;
+    let senha = document.getElementById("pSenha").value;
 
-    alert("Welcome " +nome+" "+sobreNome+ "!")
-    window.location.href = "index.html";
+    if(nome == 0){
+        alert("Digite o seu Nome")
+    } else if(sobreNome==0){
+        alert("Digite o seu SobreNome")
+    } else if(email == 0){
+        alert("Digite o seu Email")
+    } else if (senha == 0){
+        alert("Defina sua Senha")
+    } else{
+        alert("Welcome " +nome+" "+sobreNome+ "!")
+        window.location.href = "index.html";
+    }
 }
 
 
